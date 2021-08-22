830d1f712e47f2353df5d73d776e19ddbdcde0c2
SamStarlling <samelabarbosa.06@gmail.com>
2021-08-22 18:58:04 -0300

    consumindo a APi de Pets da Inara

--- src/Components/Dogs/index.jsx
+++ src/Components/Dogs/index.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 
 //Import todos os componentes do './style'
 import * as S from './Dogs.styles';
 
 const Dogs = () => {
+  const [dadosApi, setDadosApi] = useState([]);
+  const [apiChegou, setApiChegou] = useState(false)
+
+  console.log(dadosApi);
+  
+  //DidAmount
+  useEffect(() => {
+    fetch('https://salty-harbor-83280.herokuapp.com/pets/dogs')
+    .then((res) => res.json())
+    .then((data) => {
+      setDadosApi(data.results)
+      setApiChegou(true)
+    })
+  }, [])
   return (
     <S.Container>
-      Dogs
+      {!!dadosApi && apiChegou ? (dadosApi.map((item) => console.log(item.nome))) : (<p>Carregando</p>)}
     </S.Container>
   )
 }
